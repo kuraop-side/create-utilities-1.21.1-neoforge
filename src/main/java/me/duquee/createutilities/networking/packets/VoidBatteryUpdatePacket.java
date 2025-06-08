a6d986c89677cd8105a07949984661b93d84bc89
kuraop-side <jbbennett1049@gmail.com>
2025-06-07 20:40:28 -0500

    Fix NeoForge imports

---
+++ src/main/java/me/duquee/createutilities/networking/packets/VoidBatteryUpdatePacket.java
@@ -0,0 +1,45 @@
+package me.duquee.createutilities.networking.packets;
+
+import com.simibubi.create.foundation.networking.SimplePacketBase;
+
+import me.duquee.createutilities.CreateUtilitiesClient;
+import me.duquee.createutilities.blocks.voidtypes.battery.VoidBattery;
+import me.duquee.createutilities.blocks.voidtypes.motor.VoidMotorNetworkHandler.NetworkKey;
+import net.minecraft.network.FriendlyByteBuf;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.network.NetworkEvent;
+
+public class VoidBatteryUpdatePacket extends SimplePacketBase {
+
+	private final NetworkKey key;
+	private final VoidBattery battery;
+
+	public VoidBatteryUpdatePacket(NetworkKey key, VoidBattery battery) {
+		this.key = key;
+		this.battery = battery;
+	}
+
+	public VoidBatteryUpdatePacket(FriendlyByteBuf buffer) {
+		key = NetworkKey.fromBuffer(buffer);
+		battery = new VoidBattery(key);
+		battery.deserializeNBT(buffer.readNbt());
+	}
+
+	@Override
+	public void write(FriendlyByteBuf buffer) {
+		key.writeToBuffer(buffer);
+		buffer.writeNbt(battery.serializeNBT());
+	}
+
+        @Override
+        public boolean handle(NetworkEvent.Context context) {
+                context.enqueueWork(() -> {
+                        if (FMLEnvironment.dist.isClient()) {
+                                CreateUtilitiesClient.VOID_BATTERIES.storages.put(key, battery);
+                        }
+                });
+                return true;
+        }
+
+}
